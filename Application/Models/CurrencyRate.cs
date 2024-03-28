2444d3dc308424a058376716e35f904a279ebccc
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-03-28 21:02:45 +0300

    Add CurrencyRate model

---
+++ Application/Models/CurrencyRate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class CurrencyRate
+    {
+        public DateTime Date { get; set; }
+        
+        public string Abbreviation { get; set; }
+        
+        public string Name { get; set; }
+
+        public double OfficialRate { get; set; }
+    }
+}
