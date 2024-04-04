0789c6f403b970a2bae9e51811979c2218bb90b7
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-04-04 21:28:17 +0300

    Move all business logic from viewmodel layer to model layer

---
+++ Application/Model/Services/CommonService.cs
@@ -0,0 +1,68 @@
+﻿using Application.Model.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Application.Model.Services
+{
+    public class CommonService
+    {
+        private static string _apiUrl = "https://api.nbrb.by/exrates/rates?periodicity=0";
+        
+        public async Task<IList<CurrencyRate>> UploadCurrenciesFromServerAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(_apiUrl);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(responseContent);
+
+            return rates;
+        }
+
+        //private async Task UploadCurrenciesFromServer()
+        //{
+        //    string url = "https://api.nbrb.by/exrates/rates?periodicity=0";
+
+        //    HttpClient httpClient = new HttpClient();
+        //    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        //    string responseContent = await response.Content.ReadAsStringAsync();
+
+        //    IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(responseContent);
+
+        //    ShowResultMessageBox("Currency rates uploaded from server!");
+
+        //    SaveToFile(responseContent);
+        //}
+
+        //private async Task UploadCurrenciesFromFile()
+        //{
+        //    string fileContent = File.ReadAllText(_fileName);
+
+        //    IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(fileContent);
+
+        //    if (rates != null)
+        //    {
+        //        CurrenciesGridView.ItemsSource = rates;
+        //        ShowResultMessageBox("Currency rates uploaded from file!");
+        //    }
+        //    else
+        //    {
+        //        await UploadCurrenciesFromServer();
+        //    }
+        //}
+
+        //private void SaveToFile(string content)
+        //{
+        //    File.WriteAllText(_fileName, content);
+        //}
+
+        //private void ShowResultMessageBox(string message)
+        //{
+        //    MessageBox.Show(message);
+        //}
+    }
+}
--- Application/ViewModel/MainWindowViewModel.cs
+++ Application/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Model.Models;
+using Application.Model.Services;
 using Application.ViewModel.Commands;
 using Newtonsoft.Json;
 using System;
@@ -15,101 +16,32 @@ namespace Application.ViewModel
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public MainWindowViewModel()
+        {
+            _currencyRates = new ObservableCollection<CurrencyRate>();
+            _loadDataCommand = new CommonCommand(ExecuteLoadDataCommand);
+
+            _commonService = new CommonService();
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private static string _apiUrl = "https://api.nbrb.by/exrates/rates?periodicity=0";
+        public ObservableCollection<CurrencyRate> CurrencyRates => _currencyRates;
+        public ICommand LoadDataCommand => _loadDataCommand;
 
         private ObservableCollection<CurrencyRate> _currencyRates;
-        public ObservableCollection<CurrencyRate> CurrencyRates
-        {
-            get
-            {
-                if(_currencyRates is null)
-                {
-                    _currencyRates = new ObservableCollection<CurrencyRate>();
-                }
-
-                return _currencyRates;
-            }
-            //set {
-            //    _currencyRates = value;
-            //    //PropertyChanged(this, new PropertyChangedEventArgs(nameof(CurrencyRates)));
-            //}
-        }
-
         private CommonCommand _loadDataCommand;
-        public ICommand LoadDataCommand
-        {
-            get
-            {
-                return _loadDataCommand ?? (_loadDataCommand = new CommonCommand(ExecuteLoadDataCommand));
-            }
-        }
+        private readonly CommonService _commonService;
 
         private async void ExecuteLoadDataCommand(object parameter)
         {
-            IList<CurrencyRate> rates = await GetCurrenciesAwait();
+            IList<CurrencyRate> currencies = await _commonService.UploadCurrenciesFromServerAsync();
 
             _currencyRates.Clear();
-            foreach(var rate in rates)
+            foreach(var rate in currencies)
             {
                 _currencyRates.Add(rate);
             }
         }
-
-        private async Task<IList<CurrencyRate>> GetCurrenciesAwait()
-        {
-            HttpClient httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync(_apiUrl);
-
-            string responseContent = await response.Content.ReadAsStringAsync();
-
-            IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(responseContent);
-
-            return rates;
-        }
-
-        //private async Task UploadCurrenciesFromServer()
-        //{
-        //    string url = "https://api.nbrb.by/exrates/rates?periodicity=0";
-
-        //    HttpClient httpClient = new HttpClient();
-        //    HttpResponseMessage response = await httpClient.GetAsync(url);
-
-        //    string responseContent = await response.Content.ReadAsStringAsync();
-
-        //    IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(responseContent);
-
-        //    ShowResultMessageBox("Currency rates uploaded from server!");
-
-        //    SaveToFile(responseContent);
-        //}
-
-        //private async Task UploadCurrenciesFromFile()
-        //{
-        //    string fileContent = File.ReadAllText(_fileName);
-
-        //    IList<CurrencyRate> rates = JsonConvert.DeserializeObject<IList<CurrencyRate>>(fileContent);
-
-        //    if (rates != null)
-        //    {
-        //        CurrenciesGridView.ItemsSource = rates;
-        //        ShowResultMessageBox("Currency rates uploaded from file!");
-        //    }
-        //    else
-        //    {
-        //        await UploadCurrenciesFromServer();
-        //    }
-        //}
-
-        //private void SaveToFile(string content)
-        //{
-        //    File.WriteAllText(_fileName, content);
-        //}
-
-        //private void ShowResultMessageBox(string message)
-        //{
-        //    MessageBox.Show(message);
-        //}
     }
 }
